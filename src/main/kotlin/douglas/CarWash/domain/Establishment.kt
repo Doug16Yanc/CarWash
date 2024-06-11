3d12f913412480b41e1f1c280b5077d8d67f7227
douglas <douglasholanda3195@gmail.com>
2024-06-10 20:53:37 -0400

    Correction in entity and DTO

--- src/main/kotlin/douglas/CarWash/domain/Establishment.kt
+++ src/main/kotlin/douglas/CarWash/domain/Establishment.kt
@@ -1,32 +1,20 @@
 package douglas.CarWash.domain
 
-import douglas.CarWash.dto.EstablishmentDTO
 import douglas.CarWash.enumeration.VehicleStatus
-import douglas.CarWash.service.VehicleService
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "establishments")
-data class Establishment (
-
-    @Id val id : Long? = null,
-
-    var name : String? = null,
-
-    var address : Address? = null,
-
-    var telephone : String? = null,
-
-    var vehicleNumber : Int? = null,
-
-    var vehicle: MutableList<Vehicle>? = null,
-
+class Establishment(
+    @Id var id: Long?,
+    var name: String?,
+    var address: Address?,
+    var telephone: String?,
+    var vehicleNumber: Int?,
+    private var vehicle: MutableList<Vehicle>?,
+    private var vehiclesEntered: Int?,
+    private var vehiclesExited: Int?
 ) {
-    val vehiclesEntered: Int
-        get() = calculateProhibited()
-
-    val vehiclesExited: Int
-        get() = calculateExited()
 
     private fun calculateProhibited(): Int {
         return vehicle?.count { it.status == VehicleStatus.PROHIBITED } ?: 0
@@ -35,5 +23,5 @@ data class Establishment (
     private fun calculateExited(): Int {
         return vehicle?.count { it.status == VehicleStatus.EXIT } ?: 0
     }
-}
 
+}
